f51743b76180c4fa2e2a30ae66f25e42135706bd
anjali <144926934+Anjalipatel1526@users.noreply.github.com>
2025-12-02 21:04:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Home from "./pages/Home";
 import Auth from "./pages/Auth";
-import Upload from "./pages/Upload";
+import YutifyUpload from "./pages/YutifyUpload";
 import Watchlist from "./pages/Watchlist";
+import Categories from "./pages/Categories";
+import Reels from "./pages/Reels";
+import MyChannel from "./pages/MyChannel";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,11 +21,13 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
-          <Route path="/upload" element={<Upload />} />
+          <Route path="/yutify-upload" element={<YutifyUpload />} />
           <Route path="/watchlist" element={<Watchlist />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/reels" element={<Reels />} />
+          <Route path="/my-channel" element={<MyChannel />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
